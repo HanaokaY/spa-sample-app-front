4ad3e2749540227aafaab7ba7a424a3b6d7d00c4
HanaokaY <okayu5757@ezweb.ne.jp>
2022-08-03 00:28:09 +0900

    開発一時中断

--- store/auth.js
+++ store/auth.js
@@ -9,6 +9,7 @@ export const mutations = {
 };
 
 export const actions = {
+    // コンテキストオブジェクトというStoreインスタンスを受け取って、setUserという名前でmutationsを呼び出している
     setUser (context, data) {
         context.commit('setUser', data)
     }
